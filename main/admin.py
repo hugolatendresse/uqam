e8839c93d0e8c3bf728ab9d48db65ec81e080cdf
HugoLatendresse <Hugo.Latendresse@oliverwyman.com>
2021-06-05 10:01:26 -0400

    moved to uqam-django

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from quest.main.models import *
+from main.models import *
 
 # Register your models here.
 admin.site.register(Item)
--- quest/asgi.py
+++ quest/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for quest project.
+ASGI config for quest_old project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quest.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quest_old.settings')
 
 application = get_asgi_application()
