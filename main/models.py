b766b980b26a033332963cdfe2cde21ba6b86d84
HugoLatendresse <Hugo.Latendresse@oliverwyman.com>
2021-06-05 17:02:39 -0400

    conseil in define_all

--- main/models.py
+++ main/models.py
@@ -15,9 +15,22 @@ class Answer(models.Model):
     atext = models.CharField(max_length=1024)
     qnumber = models.IntegerField()
     anumber = models.IntegerField()
-
+    q_to_skip = models.CharField(max_length=1024, editable=False, default="")
     def __str__(self):
         return self.atext
+
+
+class Conseil(models.Model):
+    ctext = models.CharField(max_length=1024)
+    q1 = models.CharField(max_length=1024, editable=False, default="<any>")
+    q2 = models.CharField(max_length=1024, editable=False, default="<any>")
+    q3 = models.CharField(max_length=1024, editable=False, default="<any>")
+    q4 = models.CharField(max_length=1024, editable=False, default="<any>")
+    q5 = models.CharField(max_length=1024, editable=False, default="<any>")
+    q6 = models.CharField(max_length=1024, editable=False, default="<any>")
+
+    def __str__(self):
+        return self.ctext
 
 
 class RandomUser(models.Model):
@@ -28,6 +41,3 @@ class RandomUser(models.Model):
     q5 = models.CharField(max_length=1024, editable=False, default="Ask")
     q6 = models.CharField(max_length=1024, editable=False, default="Ask")
 
-
-class Conseil(models.Model):
-    text = models.CharField(max_length=20000)
