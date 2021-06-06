2854c59b59e11b94e654dcae26a91377725f12d3
HugoLatendresse <Hugo.Latendresse@oliverwyman.com>
2021-06-05 20:31:20 -0400

    whitesapce

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,7 @@ from main import views
 urlpatterns = [
     path('home/', views.homepage, name='home'),
     path("question", views.request_question, name='request_question'),
-
-
+    path('conseil', views.denoument, name='denoument')
     path('login/', views.loginpage, name='login'),
     path('logout/', views.logoutpage, name='logout'),
     path('register/', views.registerpage, name='register'),
